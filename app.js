ee16ada151a17529b2a1e9fffa2a5c570aba150d
Tanzim <tanzim008@gmail.com>
2023-05-29 20:26:30 -0230

    Defensive programming

--- app.js
+++ app.js
@@ -36,13 +36,43 @@
 // }
 // letterFinder('test','t');
 
-function addTwoNums(a,b){
-    try{
-        console.log(a+b);
-    }
-    catch(err){
-        console.log(err);
-    }
-}
-addTwoNums(5,"5");
+// function addTwoNums(a,b){
+//     try{
+//         if(typeof(a) != 'number'){
+//             throw new ReferenceError('the first argument is not a number');
+//         }
+//         else if(typeof(b) != 'number'){
+//             throw new ReferenceError('the second argument is not a number');
+//         }
+//         else{
+//             console.log(a+b);
+//         }
+//     }
+//     catch(err){
+//         console.log("Error!", err);
+//     }
+// }
+// addTwoNums(5,"5");
+// console.log("It still works");
 
+
+// Exercise: Defensive programming
+function letterFinder(word, match) {
+    var condition1 =  typeof(word) == 'string' && word.length >= 2;
+    var condition2 = typeof(match) == 'string' && match.length ==1;
+    if(condition1 && condition2){
+        for(var i = 0; i < word.length; i++) {
+            if(word[i] == match) {
+                //if the current character at position i in the word is equal to the match
+                console.log('Found the', match, 'at', i);
+            } else {
+                console.log('---No match found at', i);
+            }
+        }
+    }
+    else{
+        console.log( "Please pass correct arguments to the function.");
+    }
+    
+}
+letterFinder("Abnormal", "Normal"); 
