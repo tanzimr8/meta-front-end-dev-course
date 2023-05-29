d37876d68f4d0821ae1a6f67165c2d6360b74159
Tanzim <tanzim008@gmail.com>
2023-05-29 21:11:20 -0230

    Recursion

--- app.js
+++ app.js
@@ -57,22 +57,33 @@
 
 
 // Exercise: Defensive programming
-function letterFinder(word, match) {
-    var condition1 =  typeof(word) == 'string' && word.length >= 2;
-    var condition2 = typeof(match) == 'string' && match.length ==1;
-    if(condition1 && condition2){
-        for(var i = 0; i < word.length; i++) {
-            if(word[i] == match) {
-                //if the current character at position i in the word is equal to the match
-                console.log('Found the', match, 'at', i);
-            } else {
-                console.log('---No match found at', i);
-            }
-        }
-    }
-    else{
-        console.log( "Please pass correct arguments to the function.");
-    }
+// function letterFinder(word, match) {
+//     var condition1 =  typeof(word) == 'string' && word.length >= 2;
+//     var condition2 = typeof(match) == 'string' && match.length ==1;
+//     if(condition1 && condition2){
+//         for(var i = 0; i < word.length; i++) {
+//             if(word[i] == match) {
+//                 //if the current character at position i in the word is equal to the match
+//                 console.log('Found the', match, 'at', i);
+//             } else {
+//                 console.log('---No match found at', i);
+//             }
+//         }
+//     }
+//     else{
+//         console.log( "Please pass correct arguments to the function.");
+//     }
     
+// }
+// letterFinder("Abnormal", "Normal"); 
+
+// var str = "Hello";
+// str.match("jello");
+var score = 100;
+function exampleRecursion(){
+    console.log(score);
+    score = score -10;
+    if(score === 0 ) return;
+    exampleRecursion();
 }
-letterFinder("Abnormal", "Normal"); 
+exampleRecursion();
